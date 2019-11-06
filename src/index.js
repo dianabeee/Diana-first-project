0f975c26151eacef7f1a0de2a60798a56056f0b9
Diana Sousa <dianasamsousa@gmail.com>
2019-11-06 22:38:11 +0000

    Forecast Loop Done

--- src/index.js
+++ src/index.js
@@ -82,9 +82,13 @@ function formatHours(timestamp) {
 
 function displayForecast(response) {
   console.log(response.data);
-  let forecast = response.data.list[0];
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = `
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
   <div class="col-2">
     <h3>${formatHours(forecast.dt)}</h3>
     <img src="http://openweathermap.org/img/wn/${
@@ -94,32 +98,7 @@ function displayForecast(response) {
     <strong>${Math.round(forecast.main.temp_max)}º </strong>|
      ${Math.round(forecast.main.temp_min)}º</div>
   </div>`;
-
-  forecast = response.data.list[1];
-  forecastElement.innerHTML =
-    forecastElement.innerHTML +
-    `<div class="col-2">
-    <h3>${formatHours(forecast.dt)}</h3>
-    <img src="http://openweathermap.org/img/wn/${
-      forecast.weather[0].icon
-    }@2x.png" alt="icon" />
-    <div class="weather-forecast-temperature">
-    <strong>${Math.round(forecast.main.temp_max)}º </strong>|
-     ${Math.round(forecast.main.temp_min)}º</div>
-  </div>`;
-
-  forecast = response.data.list[2];
-  forecastElement.innerHTML =
-    forecastElement.innerHTML +
-    `<div class="col-2">
-    <h3>${formatHours(forecast.dt)}</h3>
-    <img src="http://openweathermap.org/img/wn/${
-      forecast.weather[0].icon
-    }@2x.png" alt="icon" />
-    <div class="weather-forecast-temperature">
-    <strong>${Math.round(forecast.main.temp_max)}º </strong>|
-     ${Math.round(forecast.main.temp_min)}º</div>
-  </div>`;
+  }
 }
 
 function search(event) {
